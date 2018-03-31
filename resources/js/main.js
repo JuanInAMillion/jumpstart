32562b37a9a75f1b131846dfb172bec44e45d518
Juan Alcantara <juan231f@yahoo.com>
2018-03-31 13:40:26 -0400

    completed slider

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    lazyLoad: 'ondemand',
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+});
