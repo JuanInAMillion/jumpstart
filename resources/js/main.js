985f6410b86504b09812269bab7909a41dfb9d0b
Juan Alcantara <juan231f@yahoo.com>
2018-04-01 23:57:46 -0400

    added a mobile logo and a desktop logo

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
     pauseOnHover: false,
   });
 
+/*  $(window).scroll(function(){
+    var x = 0;
+    var y = $(this).scrollTop();
+
+    if(y - x > 50 ) {
+      var z = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + z}, 150);
+      x === y;
+    } else if(x - y > 50) {
+      $('.navbar').animate({top: '+' + z}, 150);
+      x === y;
+    }
+  });
+*/
 });
