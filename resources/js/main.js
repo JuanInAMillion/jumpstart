1c0c38e8feecdcc4685f5fd393fcff40e799bc66
Juan Alcantara <juan231f@yahoo.com>
2018-04-04 23:17:08 -0400

    added correct scrolling code but still need to fix

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,18 +10,16 @@ $(document).ready(function() {
     pauseOnHover: false,
   });
 
-/*  $(window).scroll(function(){
-    var x = 0;
-    var y = $(this).scrollTop();
-
-    if(y - x > 50 ) {
-      var z = $('.navbar').css('height');
-      $('.navbar').animate({top: '-' + z}, 150);
-      x === y;
-    } else if(x - y > 50) {
-      $('.navbar').animate({top: '+' + z}, 150);
-      x === y;
-    }
+  var lastScrollTop = 0;
+   $(window).scroll(function(){
+     var scrollTop = $(this).scrollTop();
+     if (scrollTop - lastScrollTop > 50){
+       var navHeight = $('.navbar').css('height');
+       $('.navbar').animate({top: '-' + navHeight}, 150);
+       lastScrollTop = scrollTop;
+     } else if (lastScrollTop - scrollTop > 50) {
+       $('.navbar').animate({top: '0px'}, 150);
+       lastScrollTop = scrollTop;
+     }
   });
-*/
 });
